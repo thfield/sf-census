b6b29af4e9a1a2857ecbbc948041b13f547f1169
thfield <thfield@gmail.com>
2015-06-19 19:57:15 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var width = 960,
+    height = 960;
+
+var svg = d3.select("#map_container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("data/sf.json", function(error, sf) {
+  if (error) return console.error(error);
+
+  var sfneighborhoods = topojson.feature(sf, sf.objects.SFFind_Neighborhoods);
+
+  var projection = d3.geo.mercator()
+      .center([-122.433701, 37.767683])
+      .scale(250000)
+      .translate([width / 2, height / 2])
+      .precision(.1);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  svg.append("path")
+      .datum(sfneighborhoods)
+      .attr("d", path);
+
+  svg.selectAll(".neighborhood")
+        .data(topojson.feature(sf, sf.objects.SFFind_Neighborhoods).features)
+      .enter().append("path")
+        .attr("class", "neighborhood")
+        .attr("data-name", function(d) { return d.properties.name; })
+        .attr("d", path)
+        .attr("fill", "#fff")
+        .attr("stroke", "#000")
+
+});
